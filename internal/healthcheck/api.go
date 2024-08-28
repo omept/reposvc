d63aeac800ea786e6e21f37ed50c4a848c178325
George Onwuasoanya <georgetheprogrammer@gmail.com>
2024-08-28 08:13:04 +0100

    add / route

--- internal/healthcheck/api.go
+++ internal/healthcheck/api.go
@@ -10,6 +10,7 @@ import (
 // RegisterHandlers registers the handlers that perform healthchecks.
 func RegisterHandlers(r *mux.Router, version string) {
 	r.HandleFunc("/health", healthcheck(version))
+	r.HandleFunc("/", healthcheck(version))
 }
 
 func healthcheck(version string) func(http.ResponseWriter, *http.Request) {
